9d75aa97a725f657322dd1038c8d92b76db0c45e
Rui Rodrigues <48014356+ruifsr@users.noreply.github.com>
2021-09-15 18:41:08 +0100

    object selection bug solved
    object selection bug solved

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,29 +1,36 @@
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1200);
 const canvas = document.querySelector('#c');
+const camera = new THREE.PerspectiveCamera(75, 1024/768, 0.1, 1200);
 const renderer = new THREE.WebGLRenderer({canvas});
+let scene = new THREE.Scene();
 let objects = [];
 let controlsCam=null;
 let controlsDrag = null;
 
 window.addEventListener('change' , e=> {
+  const divHolder = document.getElementById("videoHolderDivId");
   switch(e.target.id){
     case 'radioMoveObj':
-      document.getElementById("videoHolderDivId").style.display="none";
+      divHolder.removeEventListener('click',clickRotationHandler);
+      divHolder.style.display="none";
+      document.removeEventListener("keydown", keysHandler);
       moveObj();
       break;
     case 'radioMoveCam':
-      document.getElementById("videoHolderDivId").style.display="none";
+      divHolder.removeEventListener('click',clickRotationHandler);
+      document.removeEventListener("keydown", keysHandler);
+      divHolder.style.display="none";
       moveCam();
       break;
     case 'radioRotate':
-      if(e.target.checked){
-        document.getElementById("videoHolderDivId").style.display="block";
-      } else {
-        document.getElementById("videoHolderDivId").style.display="none";
-      }
+        pickHelper.PickedObject=null;
+        while (divHolder.firstChild) divHolder.removeChild(divHolder.lastChild);
+        divHolder.addEventListener('click',clickRotationHandler);
+        divHolder.style.display="block";
     break;
     case 'radioNone':
-      document.getElementById("videoHolderDivId").style.display="none";
+      divHolder.removeEventListener('click',clickRotationHandler);
+      document.removeEventListener("keydown", keysHandler);
+      divHolder.style.display="none";
     break;
   }
 });
@@ -64,7 +71,7 @@ function scale(){
 }
 
 function main() {
-    let scene = new THREE.Scene();
+    
     camera.position.z=200;
 
     let geometry = new THREE.BoxGeometry(1000,1000,1000);
@@ -83,11 +90,11 @@ function main() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById("container").appendChild(renderer.domElement);
 
-    window.addEventListener('resize', () => { 
+    /*window.addEventListener('resize', () => { 
       renderer.setSize(window.innerWidth, window.innerHeight); 
       camera.aspect = window.innerWidth/window.innerHeight;
       camera.updateProjectionMatrix();
-    });
+    });*/
 
     let keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30,100%,75%)'), 1.0);
     keyLight.position.set(-100,0,100);
@@ -154,24 +161,6 @@ function main() {
         gltfStore.mixer.clipAction(object.animations[0]).play();
       });
 
-      //rotation
-      const pickPosition = {x: 100000, y: 100000}; //distant positions for start
-
-      document.getElementById('container').addEventListener('click', (event)=>{    
-        const pos = getCanvasRelativePosition(event);        // setPickPosition
-        pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
-        pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
-        pickHelper.pick(pos,pickPosition, scene, camera);
-      });
-    
-      function getCanvasRelativePosition(event) {
-        const rect = canvas.getBoundingClientRect();
-        return {
-          x: (event.clientX - rect.left) * canvas.width  / rect.width,
-          y: (event.clientY - rect.top ) * canvas.height / rect.height,
-        };
-      }
-
     //gameLogic
     function update(){
       //cube.rotation.x+=0.01;
@@ -213,23 +202,10 @@ class PickHelper {
   get PickedObject(){return this.pickedObject;}
   set PickedObject(value){this.pickedObject=value;}
   pick(positions, normalizedPosition, scene, camera) {
-    var self=this;
     // cast a ray through the frustum
     this.raycaster.setFromCamera(normalizedPosition, camera);
     // get the list of objects the ray intersected
-    const intersectedObjects = this.raycaster.intersectObjects(scene.children);
-    //escape 27//left = 37//up = 38//right = 39//down = 40//escape event 25
-    function keysHandler(ev) {
-      ev = ev || window.event;
-      if (ev.keyCode == 27) {
-        self.pickedObject = null; 
-        deleteArrows();
-        document.removeEventListener("keydown", keysHandler);
-      } else if (ev.keyCode == 37) { self.pickedObject.rotation.y+=0.2;
-      } else if (ev.keyCode == 38) { self.pickedObject.rotation.x-=0.2;
-      } else if (ev.keyCode == 39) { self.pickedObject.rotation.y-=0.2;
-      } else if (ev.keyCode == 40) { self.pickedObject.rotation.x+=0.2;
-    }}
+    const intersectedObjects = this.raycaster.intersectObjects(scene.children, true);
     
     if (this.pickedObject && !intersectedObjects.length) {
       this.pickedObject = null;
@@ -266,8 +242,8 @@ function addImg(x, y, src, pickedObject, keysHandler) {
   let fatherNode = document.getElementById('videoHolderDivId')
   let img = document.createElement('img');
   img.className = 'arrow';
-  img.style.top = ((y*100)/fatherNode.clientWidth) + "%";
-  img.style.left = ((x*100)/fatherNode.clientHeight) + "%";
+  img.style.top = y+"px";
+  img.style.left = x + "px";
   img.src = src;
   img.addEventListener('mousedown', ()=>{
     if(img.src.includes("up")){pickedObject.rotation.x-=0.2;
@@ -280,6 +256,39 @@ function addImg(x, y, src, pickedObject, keysHandler) {
       document.removeEventListener("keydown", keysHandler);
   }});
   fatherNode.appendChild(img);
+}
+
+//----------------------
+
+//escape 27//left = 37//up = 38//right = 39//down = 40//escape event 25
+function keysHandler(ev) {
+  ev = ev || window.event;
+  if (ev.keyCode == 27) {
+    pickHelper.pickedObject = null; 
+    deleteArrows();
+    document.removeEventListener("keydown", keysHandler);
+  } else if (ev.keyCode == 37) { pickHelper.pickedObject.rotation.y+=0.2;
+  } else if (ev.keyCode == 38) { pickHelper.pickedObject.rotation.x-=0.2;
+  } else if (ev.keyCode == 39) { pickHelper.pickedObject.rotation.y-=0.2;
+  } else if (ev.keyCode == 40) { pickHelper.pickedObject.rotation.x+=0.2;
+}}
+
+//--------------------------------------------------------------------------
+
+function clickRotationHandler(event){ 
+  const pickPosition = {x: 100000, y: 100000}; //distant positions for start   
+  const pos = getCanvasRelativePosition(event);        // setPickPosition
+  pickPosition.x = (pos.x / canvas.clientWidth ) *  2 - 1;
+  pickPosition.y = (pos.y / canvas.clientHeight) * -2 + 1;  // note we flip Y
+  pickHelper.pick(pos,pickPosition, scene, camera);
+
+  function getCanvasRelativePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: (event.clientX - rect.left) * canvas.width  / rect.width,
+      y: (event.clientY - rect.top ) * canvas.height / rect.height,
+    };
+  }
 }
 
 const pickHelper = new PickHelper();
